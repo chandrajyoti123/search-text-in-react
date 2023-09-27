704a0625c2b011b97fa02ebc9a026943f557311e
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-09-27 21:37:19 +0530

    added whatsapp folder

--- src/index.js
+++ src/index.js
@@ -129,6 +129,6 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <>
-        <RouterProvider router={router} />
+        <RouterProvider router={router}  />
    </>
 );
