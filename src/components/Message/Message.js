1022aebd3a554e702f34c5190b775dd1e721dd48
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-09-29 18:54:47 +0530

    made some changes

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -2,6 +2,9 @@ import './Message.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faXmark,faPen } from "@fortawesome/free-solid-svg-icons";
 const Message=({text,deletefun,obj,editmessage,index,id})=>{
+    const date = new Date();
+      const hourse=date.getHours()
+      const minute=date.getMinutes()
     return(
         <div className="message" id={id}>
             <span><FontAwesomeIcon icon={faXmark} className='delete' onClick={()=>{
@@ -10,7 +13,9 @@ const Message=({text,deletefun,obj,editmessage,index,id})=>{
             <span><FontAwesomeIcon icon={faPen} className='edit' onClick={()=>{
                 editmessage(index)
             }}/></span>
+
            {text}
+           <div className='timeofmessage'>{`${hourse}:${minute}pm`}</div>
 
         </div>
     )
